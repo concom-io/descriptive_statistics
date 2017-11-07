0c7cd7f00aac8d156c3f9282fbe45cc07f1e4377
David Mazvovsky <david.mazvovsky@gmail.com>
2017-11-07 17:06:46 +0200

    Update sum.rb

--- lib/descriptive_statistics/sum.rb
+++ lib/descriptive_statistics/sum.rb
@@ -1,5 +1,5 @@
 module DescriptiveStatistics
-  def sum(collection, &block)
+  def sum(collection=nil, &block)
     collection ||= self
     values = Support::convert(collection, &block)
     return DescriptiveStatistics.sum_empty_collection_default_value if values.empty?
